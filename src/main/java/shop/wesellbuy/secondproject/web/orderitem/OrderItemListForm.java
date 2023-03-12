db7f47e609027b1884a4a7fae2a0371f6ff8f696
coderwin <gommind@naver.com>
2023-03-12 15:20:06 +0900

    update orderItem

--- src/main/java/shop/wesellbuy/secondproject/web/orderitem/OrderItemListForm.java
+++ src/main/java/shop/wesellbuy/secondproject/web/orderitem/OrderItemListForm.java
@@ -10,10 +10,12 @@ import shop.wesellbuy.secondproject.domain.member.Phone;
  * 주문상품 목록 dto
  * writer : 이호진
  * init : 2023.02.04
- * updated by writer :
- * update :
+ * updated by writer : 이호진
+ * update : 2023.03.12
  * description : 서버에서 보내온 판매된 상품의 주문상품 정보를 담아둔다.
  *               -> 판매자가 자신이 올린 상품의 판매된 정보를 확인한다.
+ *
+ * update : > orderNum(주문번호) 추가
  */
 @Getter
 @AllArgsConstructor
@@ -29,6 +31,7 @@ public class OrderItemListForm {
     private Address address; // 배송지
     private int itemNum; // 상품 번호
     private int totalCount; // 주문 총가격
+    private int orderNum; // 주문 번호
 
     // ** 생성 메서드 ** //
     public static OrderItemListForm create(OrderItem orderItem) {
@@ -42,7 +45,8 @@ public class OrderItemListForm {
                 orderItem.getOrder().getMember().getPhones(),
                 orderItem.getOrder().getDelivery().getAddress(),
                 orderItem.getItem().getNum(),
-                orderItem.getTotalPrice()
+                orderItem.getTotalPrice(),
+                orderItem.getOrder().getNum()
         );
 
         return form;
